849c5753d54dde41f9262ab53c1efd82c98b6cd7
yangyu <yangyu@httc.com.cn>
2016-04-20 18:24:07 +0800

    add keys

--- tools/clean-ansibel-connection.sh
+++ tools/clean-ansibel-connection.sh
@@ -5,4 +5,4 @@ set -x
 rm /tmp/ansible-*
 rm ~/.ansible -rf
 rm ~/.ssh/known_hosts
-sshpass -p httc ssh root@10.0.2.107 "rm -rf /root/.ansible"
+sshpass -p httc ssh root@10.0.2.137 "rm -rf /root/.ansible"

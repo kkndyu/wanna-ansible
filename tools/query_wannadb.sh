df8987895e598d3506536a9c0897b3200364231b
yangyu <yangyu@httc.com.cn>
2016-05-05 16:21:30 +0800

    add reprepro role

--- tools/query_wannadb.sh
+++ tools/query_wannadb.sh
@@ -2,5 +2,5 @@
 
 set -x
 
-#sudo -u buildd ssh wbadm@10.0.2.107 wanna-build --dist=jessie -A amd64 --list=$1
+sudo -u buildd ssh wbadm@10.0.2.107 wanna-build --dist=jessie -A amd64 --list=$1
 sudo -u buildd ssh wbadm@10.0.2.107 /srv/wanna-build/bin/wanna-build-statistics --dist=jessie --database=amd64

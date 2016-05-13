a54f3406ef2752cb1bffd54e2de227b28bd0a5bb
yangyu <yangyu@httc.com.cn>
2016-05-13 12:37:40 +0800

    custom module output log line by line

--- library/trigger_local.sh
+++ library/trigger_local.sh
@@ -21,6 +21,15 @@ source ${1}   # Very, *very*, dangerous!
 cd /srv/wanna-build/triggers
 . common
 
+#
+# There are two exec command in this script
+# the 1st redirect &1 &2 to file
+# the last restore it back
+#
+# Ansible module need json as return value 
+# If stdout or stderr output to standard
+# json will invalid and moudle will failed
+#
 exec > $ARCHIVE_BASE/trigger.log 2>&1
 echo "`date`: Running trigger for $ARCHIVE ..." 
 
@@ -72,9 +81,31 @@ main() {
 }
 
     #"log": $(cat $ARCHIVE_BASE/trigger.log)
+    #"log": "$(cat $ARCHIVE_BASE/trigger.log | tr "\n" "+" | sed "s/’//g" | sed "s/‘//g")"
     #"log": "Thu May 12 23:43:34 CST 2016: Running trigger for local ...+--"
+
+
+
+
 main
+
+#
+# change stdout to json list
+# add "" to the beginning and ending in each line
+# replace \n to ,\n
+# add [] to the b/e of the whole log
+# like:
+# [
+#     "line",
+#     "second line"
+# ]
+#
+echo "[" > $ARCHIVE_BASE/trigger.log.list
+awk -vRS='\n' -vORS=',\n' '{NF=NF;print "\t""\""$0"\""}' $ARCHIVE_BASE/trigger.log >> $ARCHIVE_BASE/trigger.log.list
+echo -e "\t\"done\"\n]" >> $ARCHIVE_BASE/trigger.log.list
+
 exec 1>/dev/tty
+
 if [ $? -eq 0 ]; then
     #echo "{\"changed\":true, \"msg\": \"OK\", \"log\": \"$(cat $ARCHIVE_BASE/trigger.log)\"}"
     #echo "{\"changed\":true, \"msg\": \"OK\"}"
@@ -82,7 +113,9 @@ if [ $? -eq 0 ]; then
 {
     "changed": true,
     "msg": "OK",
-    "log": "$(cat $ARCHIVE_BASE/trigger.log | tr "\n" "+" | sed "s/’//g" | sed "s/‘//g")"
+    "stdout": "$(cat $ARCHIVE_BASE/trigger.log | sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/\\n/g')",
+    "stdout_lines": $(cat $ARCHIVE_BASE/trigger.log.list)
+
 }
 EOF
     exit 0
